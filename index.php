2fe26c772a63c674bb247c42eb94834adf59966e
Kevin Dilts <kdilts13@gmail.com>
2016-09-17 16:40:46 -0600

    Added a creation date field to both classes.

--- index.php
+++ index.php
@@ -5,24 +5,25 @@
 	<body>
 		<?php
 		echo "<h1>PHP Objects</h1>";
-		$user1 = new User("bill", "asdf");
+		$user1 = new User("bill", "asdf",date("m/d/Y"));
 		print_r($user1);
 		echo "<br>";
 		$user1 = null;
 		print_r("print_r on null object: " . $user1);
 		echo "<br><br>";
-		$subscriber1 = new Subscriber("Jeff","jkl;", "555-5555", "jeff@fake.net");
+		$subscriber1 = new Subscriber("Jeff","jkl;",date("m/d/Y"), "555-5555", "jeff@fake.net");
 		print_r($subscriber1);
 		echo "<br>";
 		$subscriber1 = null;
 
 		class User
 		{
-			public $name, $password;
+			public $name, $password, $createdOn;
 
-			function __construct($name,$password){
+			function __construct($name,$password,$date){
 				$this->name = $name;
 				$this->password = $password;
+				$this->createdOn = $date;
 				$this->display();
 			}
 
@@ -32,7 +33,7 @@
 
 			function display()
 			{
-				echo "name: $this->name password: $this->password<br>";
+				echo "name: $this->name password: $this->password Created on: $this->createdOn<br>";
 			}
 		}
 
@@ -40,11 +41,11 @@
 		{
 			public $phone, $email;
 
-			function __construct($name, $password, $phone, $email)
+			function __construct($name, $password, $date, $phone, $email)
 			{
 				$this->phone = $phone;
 				$this->email = $email;
-				parent::__construct($name, $password);
+				parent::__construct($name, $password, $date);
 			}
 
 			function display()
