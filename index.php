2baf83a9659b9a6a108f6f43adf68ead4a2a93ad
Kevin Dilts <kdilts13@gmail.com>
2016-09-17 15:37:07 -0600

    Created a class with constructor and tested out the print_r function.

--- index.php
+++ index.php
@@ -5,6 +5,23 @@
 	<body>
 		<?php
 		echo "<h1>PHP Objects</h1>";
+		print_r(new User("bill", "asdf"));
+
+		class User
+		{
+			public $name, $password;
+
+			function __construct($name,$password){
+				$this->name = $name;
+				$this->password = $password;
+				$this->display();
+			}
+
+			function display()
+			{
+				echo "name: $this->name password: $this->password<br>";
+			}
+		}
 		?>
 	</body>
 </html>
